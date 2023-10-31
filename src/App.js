642889b73b94c270ff9c699ce989c05a54ae1efb
characterMi <abol1385fx@gmail.com>
2023-10-31 04:22:16 +0330

    fix: fix the routing problem

--- src/App.js
+++ src/App.js
@@ -12,20 +12,22 @@ const App = () => {
     const [loading, setLoading] = useState(false)
 
     return (
-        <HelmetProvider>
-            <CustomScroll />
-            <Box width="400px" sx={{ width: { xl: "1488px" }, m: "auto" }}>
-                <Helmet>
-                    <title>Gym Lovers</title>
-                </Helmet>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home loading={loading} setLoading={setLoading} />} />
-                    <Route path="/exercise/:id" element={<ExerciseDetail loading={loading} setLoading={setLoading} />} />
-                </Routes>
-                <Footer />
-            </Box>
-        </HelmetProvider>
+        <>
+            <HelmetProvider>
+                <CustomScroll />
+                <Box width="400px" sx={{ width: { xl: "1488px" }, m: "auto" }}>
+                    <Helmet>
+                        <title>Gym Lovers</title>
+                    </Helmet>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/" element={<Home loading={loading} setLoading={setLoading} />} />
+                        <Route path="/exercise/:id" element={<ExerciseDetail loading={loading} setLoading={setLoading} />} />
+                    </Routes>
+                    <Footer />
+                </Box>
+            </HelmetProvider>
+        </>
     )
 }
 
