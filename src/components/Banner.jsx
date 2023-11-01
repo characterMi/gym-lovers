ddd5d50ddd9ddd0ef0b891be5d39c2f7d408025a
characterMi <abol1385fx@gmail.com>
2023-11-01 19:54:08 +0330

    style: make this site more responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,14 @@ import BannerImg from "../assets/images/banner.jpeg"
 
 const Banner = () => {
   return (
-    <Box sx={{ mt: { md: "212px", xs: "70px" }, ml: { sm: "50px" } }} position="relative" p="20px">
+    <Box
+      sx={{
+        mt: { md: "212px", xs: "70px" },
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
       <Typography color="#ff2625" fontWeight="600" fontSize="26px">
         Fitness Club
       </Typography>
@@ -15,7 +22,7 @@ const Banner = () => {
         Check out the most effective Exercises
       </Typography>
       <Button sx={{ background: "#ff2625", padding: "10px" }} onClick={() => window.scrollTo({ top: window.innerHeight * 1.8, behavior: "smooth" })} variant="contained" color="error">Explore Exercises</Button>
-      <Typography fontWeight={600} color="#ff2625" sx={{ opacity: 0.1, fontSize: { lg: "200px", xs: "50px", sm: "100px" } }} mb="23px" mt="100px">Exercise</Typography>
+      <Typography fontWeight={600} color="#ff2625" sx={{ opacity: {xs: 0.3, md: 0.1}, fontSize: { lg: "200px", xs: "50px", sm: "100px" } }} mb="23px" mt="120px">Exercise</Typography>
       <img src={BannerImg} alt="Banner" className="banner-img" loading="lazy" />
     </Box>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Box, Button, Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 import Logo from "../assets/images/logo.png";
 
@@ -9,9 +9,9 @@ const Navbar = () => {
       <Link to="/">
         <Box component="img" src={Logo} alt="Logo" sx={{ height: { xs: "56px", sm: "68px" }, m: { xs: "0", sm: "0 20px" } }} />
       </Link>
-      <Stack direction="row" gap="40px" sx={{ fontSize: { sm: "24px", xs: "20px" } }} alignItems="flex-end">
+      <Stack direction="row" gap="25px" sx={{ fontSize: { sm: "24px", xs: "20px" } }} alignItems="flex-end">
         <Link to="/" style={{ textDecoration: "none", color: "#3a1212", borderBottom: "3px solid #ff2625" }}>Home</Link>
-        <Button variant='text' color='error' sx={{ color: "#3a1212", fontWeight: "bold", mb: "-10px" }} onClick={() => window.scrollTo({ top: window.innerHeight * 2 - 100, behavior: "smooth" })}>Exercises</Button>
+        <button style={{ color: "#3a1212", fontWeight: "500", marginBottom: "-5px", fontSize: "20px", border: "none", outline: "none", background: "none" }} onClick={() => window.scrollTo({ top: window.innerHeight * 2 - 100, behavior: "smooth" })}>Exercises</button>
       </Stack>
     </Stack>
   )
