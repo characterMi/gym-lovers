ef0215323fff033c443273b120b6cc71ed6203d6
characterMi <abol1385fx@gmail.com>
2023-11-05 15:49:54 +0330

    hide scrollbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       </Link>
       <Stack direction="row" gap="25px" sx={{ fontSize: { sm: "24px", xs: "20px" } }} alignItems="flex-end">
         <Link to="/" style={{ textDecoration: "none", color: "#3a1212", borderBottom: "3px solid #ff2625" }}>Home</Link>
-        <button style={{ color: "#3a1212", marginBottom: "-5px", fontSize: "20px", border: "none", outline: "none", background: "none" }} onClick={() => window.scrollTo({ top: window.innerHeight * 2 - 100, behavior: "smooth" })}>Exercises</button>
+        <button style={{ color: "#3a1212", cursor: "pointer", marginBottom: "-3px", fontSize: "20px", border: "none", outline: "none", background: "none" }} onClick={() => window.scrollTo({ top: window.innerHeight * 2 - 100, behavior: "smooth" })}>Exercises</button>
       </Stack>
     </Stack>
   )
--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </React.StrictMode>
+    <HashRouter>
+        <App />
+    </HashRouter>
 )
