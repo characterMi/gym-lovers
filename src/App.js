da34f1f43536cf95892ebd423c1b15a76b1bb39d
characterMi <abol1385fx@gmail.com>
2024-12-30 23:38:07 +0330

    fix: fix performance w lazy and accessibility

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Box } from "@mui/material";
-import { useState } from "react";
+import { lazy, useState } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import { CustomScroll, Footer, Navbar } from "./components";
-import ExerciseDetail from "./pages/ExerciseDetail";
-import Home from "./pages/Home";
+
+import "./App.css";
+
+const ExerciseDetail = lazy(() => import("./pages/ExerciseDetail"));
+const Home = lazy(() => import("./pages/Home"));
 
 const App = () => {
   const [loading, setLoading] = useState(false);
